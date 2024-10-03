1fd1a2bb6ff59efd2cf685885cfe02be69e48577
Levi Aho <leviaho88@gmail.com>
2024-10-03 07:07:29 -0400

    Framing the header and Nav

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+const formBanner = document.querySelector("div");
+
+formBanner.textContent = "Hello formers";
+
+
+let listArray = document.getElementById("listArray");
+
+function listItems(){
+    const
+}
+
+*/
